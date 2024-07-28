1f042061aa544128168d839b9706dad74ae91d23
Vinay Sharma <vinaykumarred7@gmail.com>
2024-07-28 22:36:33 +0530

    change value of variable a

--- conditionalOperator.js
+++ conditionalOperator.js
@@ -2,7 +2,7 @@
 The conditional operator is represented by a question mark ?. Sometimes it’s called “ternary”, because the operator has three operands. It is actually the one and only operator in JavaScript which has that many.
 */
 
-let a = 100;
+let a = 200;
 let b;
 (a == 100)? b = "true" : b = "false";
 console.log(b);
