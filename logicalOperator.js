01beacdfa114223dccc13ee744853f696969fd65
Vinay Sharma <vinaykumarred7@gmail.com>
2024-07-31 01:52:20 +0530

    logicalOperatorPractice

---
+++ logicalOperator.js
@@ -0,0 +1,62 @@
+// //Logical operators
+// JavaScript has three main logical operators:
+
+
+// Logical AND (&&): Returns true if both operands are true.
+
+
+// let a = 27;
+// if(a > 20 && a < 25){
+//     console.log("hello")
+// };
+
+// let age = 19;
+// if(age >= 18 && age <= 26){
+//     console.log("you are eligible")
+// }else{
+//     console.log("try again")
+// };
+
+
+// let age = 27;
+// if(age >= 18 && age <= 26){
+//     console.log("you are eligible")
+// }else if(age <= 17){
+//     console.log("try after 18");
+// }else{
+//     console.log("Too Old")
+// }
+
+// Logical OR (||): Returns true if at least one operand is true.
+
+// let hour = 10;
+// let hour2 = 50;
+// if(hour > 10 || hour2 < 18){
+//     console.log("the office is closed.")
+// }
+
+
+// Logical NOT (!): Returns the opposite boolean value of the operand.
+
+// console.log(!true)
+// console.log(!0)
+
+// let a = 15;
+// if(!a > 14){
+//     console.log("Hello there")
+// }else{
+//     console.log("error")
+// };
+
+//A double NOT !! is sometimes used for converting a value to boolean type:
+
+console.log(!!"hello")
+console.log(!"hello")
+
+
+
+
+
+
+
+
