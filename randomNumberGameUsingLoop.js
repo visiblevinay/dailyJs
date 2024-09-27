0d28ba5c140e63747986a2646f1551f4895a91ed
Vinay Sharma <vinaykumarred7@gmail.com>
2024-09-27 23:55:52 +0530

    numberGuessingGameUsingLoops

---
+++ randomNumberGameUsingLoop.js
@@ -0,0 +1,23 @@
+//number guess game using forLopp:-
+
+// let gameNum = 5;
+
+// for (let userNum = prompt("Guess A Number Between 0 To 10 :"); userNum != gameNum;
+//  userNum = prompt("Oops, Try Again :( ")) {  
+// }
+//  // Loop will continue to re-prompt until the correct number is guessed.
+
+// document.write("<h1>Congratulations! You guessed the right number.</h1>");
+
+
+//Same code modified in whileLoop:-
+
+// let gameNum = 5;
+// let userNum = prompt("Guess A Number Between 0 To 10 :");
+
+// while (userNum != gameNum) {
+//      userNum = prompt("Oops Try Again :( ");
+// }
+ // Loop will continue to re-prompt until the correct number is guessed.
+
+// document.write("<h1>Congratulation You Guess The Right Number.</h1>");
